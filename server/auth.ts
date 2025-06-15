613e3572e46fe552c30942c35032163ac93331f3
ZinHt9tHlaing <phyoearkar267@gmail.com>
2025-06-15 23:23:41 +0630

    next auth configuration

---
+++ server/auth.ts
@@ -0,0 +1,10 @@
+import NextAuth from "next-auth";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { db } from "@/server";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: DrizzleAdapter(db),
+  secret: process.env.NEXTAUTH_SECRET,
+  session: { strategy: "jwt" },
+  providers: [],
+});
